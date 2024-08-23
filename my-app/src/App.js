dcaee3d0ee33eadcf193b3ab86802c261f445fab
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2024-08-23 19:32:52 -0300

    criando a home
    adicionei a primeira section e fiz um css root para guardar as cores usadas durante todo o projeto.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,15 +5,12 @@ import Modulo1 from './components/pages/modules-pages/Modulo1'
 import Modulo2 from './components/pages/modules-pages/Modulo2'
 import Modulo3 from './components/pages/modules-pages/Modulo3'
 import Modulo4 from './components/pages/modules-pages/Modulo4'
-// import Sobre from './components/pages/Sobre'
-// import Contato from './components/pages/Contato'
-// import NovoProjeto from './components/pages/NovoProjeto'
-// import Projetos from './components/pages/Projetos'
 
 import Container from './components/layout/Container'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import About from './components/modules/About'
+import Home from './components/modules/Home'
 
 
 function App() {
@@ -22,6 +19,7 @@ function App() {
       <Navbar />
       <Container customClass='min-height'>
         <Routes>
+            <Route exact path="/" element= {<Home />}/>
             <Route exact path="/modulos" element= {<Modules />}/>
             <Route exact path="/modulos/modulo-1" element= {<Modulo1 />}/>
             <Route exact path="/modulos/modulo-2" element= {<Modulo2 />}/>
---
+++ my-app/src/components/modules/Home.js
@@ -0,0 +1,25 @@
+import Container from '../layout/Container'
+import React from 'react'
+import styles from './Home.module.css'
+import imageBackground from '../../img/game-images/game2.jpeg'
+import paisonCodeLogo from '../../img/game-images/paisonCodeLogo.png'
+
+function Home() {
+    return (
+        <section className={styles.homepage}>
+            <div className={styles.backgroundOverlay}>
+                <img src={imageBackground} className={styles.backgroundImage} alt="Background" />
+            </div>
+            <img src={paisonCodeLogo} className={styles.logo} alt="Paison Code Logo" />
+            <div className={styles.title}>
+                <h1>SEJA BEM-VINDO</h1>
+            </div>
+            <div className={styles.text}>
+                <p>Neste mundo incrível de aventura e educação, evolua suas habilidades em programação com Python, encare grandes desafios e se torne o Dev Mestre que tem dentro de você.</p>
+            </div>
+            <a href='/modulos'>ENTRAR AGORA</a>
+        </section>
+    )
+}
+
+export default Home;
